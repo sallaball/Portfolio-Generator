3ac1c3c3adcda1ab3d4eca29d1ccae330285a3ef
sallaball <sallaball1206@gmail.com>
2022-07-28 07:47:39 +0300

    added npm

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const generatePage = require('./src/page-template');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const pageHMTL = generatePage(name, github);
 
 
+// fs.writeFile('./index.html', pageHMTL, err => {
+//   if (err) throw err;
 
-// const profileDataArgs = process.argv.slice(2);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-
-
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
